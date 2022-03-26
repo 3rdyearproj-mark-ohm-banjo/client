1d2d13c717a27e6b4056f9091f61690ee346b5c4
Thanapat Suwannaard <thanapat@dek-d.com>
2022-03-26 14:10:37 +0700

    basic config icon

---
+++ config/icon.js
@@ -0,0 +1,19 @@
+import { faCheck } from '@fortawesome/free-solid-svg-icons'
+
+export const ICONS = { faCheck }
+export const ICON_SIZE = {
+  xs: 'xs',
+  sm: 'sm',
+  md: 'md',
+  lg: 'lg',
+  xl: 'xl',
+  '2x': '2x',
+  '3x': '3x',
+  '4x': '4x',
+  '5x': '5x',
+  '6x': '6x',
+  '7x': '7x',
+  '8x': '8x',
+  '9x': '9x',
+  '10x': '10x',
+}
