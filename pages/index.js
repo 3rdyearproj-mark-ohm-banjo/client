d3cc03694bd0fc20618b53c918de6bc3240097ee
punthanat <punthanat@hotmail.com>
2022-05-30 11:06:37 +0700

    [Test] test01

--- pages/index.js
+++ pages/index.js
@@ -178,7 +178,7 @@ const Home = ({newBook, recommendBook}) => {
   return (
     <>
       <Head>
-        <title>Share my Book Test01</title>
+        <title>Share my Book</title>
         <meta
           name="description"
           content="INT365/371 Share my Book Application"
@@ -203,7 +203,7 @@ const Home = ({newBook, recommendBook}) => {
 
             <AppDescribe>
               <Trail open={true}>
-                <AppName>Share My Book</AppName>
+                <AppName>Share My Book Test01</AppName>
                 <Divider lineColor={COLORS.PRIMARY}></Divider>
                 <span>แอปพลิเคชันส่งต่อหนังสือ </span>
               </Trail>
