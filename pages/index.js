6949033c5c64a9ad888521043a752cef5f192940
punthanat <punthanat@hotmail.com>
2022-06-01 18:05:53 +0700

    [Update] test

--- pages/index.js
+++ pages/index.js
@@ -203,7 +203,7 @@ const Home = ({newBook, recommendBook}) => {
 
             <AppDescribe>
               <Trail open={true}>
-                <AppName>Share My Book Test05</AppName>
+                <AppName>Share My Book</AppName>
                 <Divider lineColor={COLORS.PRIMARY}></Divider>
                 <span>แอปพลิเคชันส่งต่อหนังสือ </span>
               </Trail>
